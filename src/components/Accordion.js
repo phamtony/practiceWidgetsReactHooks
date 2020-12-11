221d478c4c480196e80cf8fef945c16c2ebb9cff
Tony Pham <tony.p@lashowroom.com>
2020-12-10 23:18:40 -0800

    understaing react hooks with useState, class equavalent of function component states

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, {useState} from "react";
 
 const Accordion = ({items}) => {
+    const [activeIndex, setActiveIndex] = useState(null);
+
     const onTitleClick = (index) => {
-        console.log(index);
+        setActiveIndex(index);
     };
 
     const renderedItems = items.map((item, index) => {
@@ -24,6 +26,7 @@ const Accordion = ({items}) => {
     return (
         <div className="ui styled accordion">
             {renderedItems}
+            <h1>{activeIndex}</h1>
         </div>
     )
 }
