78a49510d616393aca593b2f065542607935ea5d
Tony Pham <tony.p@lashowroom.com>
2020-12-16 12:17:04 -0800

    detecting url changes with popstate

--- src/components/Route.js
+++ src/components/Route.js
@@ -1,6 +1,18 @@
-import React from "react";
+import React, {useEffect} from "react";
 
 const Route = ({path, children}) => {
+    useEffect(() => {
+        const onLocationChange = () => {
+            console.log('Location Change');
+        }
+
+        window.addEventListener('popstate', onLocationChange);
+
+        return () => {
+            window.removeEventListener('popstate', onLocationChange)
+        }
+    }, []);
+
     return window.location.pathname === path ? children : null;
 };
 
